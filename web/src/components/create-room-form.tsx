ab78ce869a05b6c69569cd8d16b7f3179529e7a0
Laura de Oliveira <l4ur4.88@gmail.com>
2025-07-10 19:53:04 -0300

    fix: Reload room list after form submit

--- web/src/components/create-room-form.tsx
+++ web/src/components/create-room-form.tsx
@@ -41,6 +41,8 @@ export function CreateRoomForm() {
 
   async function handleCreateRoom({ name, description }: CreateRoomFormData) {
     await createRoom({ name, description });
+
+    createRoomForm.reset();
   }
 
   return (
