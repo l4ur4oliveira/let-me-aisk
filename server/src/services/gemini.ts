8543f5697df24a9d122e59d3c6cab24b11e34f80
Laura de Oliveira <l4ur4.88@gmail.com>
2025-07-12 16:00:00 -0300

    feat: Generate an answer based on the question and the audio transcription

--- server/src/services/gemini.ts
+++ server/src/services/gemini.ts
@@ -1,9 +1,9 @@
-import { GoogleGenAI } from '@google/genai'
+import { GoogleGenAI } from '@google/genai';
 import { env } from '../../infra/env.ts';
 
 const gemini = new GoogleGenAI({
   apiKey: env.GEMINI_API_KEY,
-})
+});
 
 const model = 'gemini-2.5-flash';
 
@@ -17,11 +17,11 @@ export async function transcribeAudio(audioAsBase64: string, mimeType: string) {
       {
         inlineData: {
           mimeType,
-          data: audioAsBase64
-        }
-      }
-    ]
-  })
+          data: audioAsBase64,
+        },
+      },
+    ],
+  });
 
   if (!response.text) {
     throw new Error('Failed to transcribe audio.');
@@ -36,12 +36,51 @@ export async function generateEmbeddings(text: string) {
     contents: [{ text }],
     config: {
       taskType: 'RETRIEVAL_DOCUMENT',
-    }
-  })
+    },
+  });
 
   if (!response.embeddings?.[0].values) {
     throw new Error('Failed to generate embeddings.');
   }
 
   return response.embeddings[0].values;
-} 
+}
+
+export async function generateAnswer(
+  question: string,
+  transcriptions: string[]
+) {
+  const context = transcriptions.join('\n\n');
+
+  const prompt = `
+    Com base no texto fornecido abaixo como contexto, responda à pergunta de forma clara e precisa em português do Brasil.
+
+    CONTEXTO:
+    ${context}
+
+    PERGUNTA:
+    ${question}
+
+    INSTRUÇÕES:
+    - Use apenas informações contidas no contexto enviado;
+    - Se a resposta não for encontrada no contexto, responde apenas que não possui informações suficientes;
+    - Seja objetivo;
+    - Mantenha um tom educativo e profissional;
+    - Cite trechos relevantes do contexto, se necessário;
+  `.trim();
+
+  const response = await gemini.models.generateContent({
+    model,
+    contents: [
+      {
+        text: prompt,
+      },
+    ],
+  });
+
+  if (!response.text) {
+    throw new Error('Failed to generate an answer.');
+  }
+
+  return response.text;
+}
