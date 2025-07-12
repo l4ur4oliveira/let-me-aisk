a4ad339b1166d55c7e98d6f7d9e3ac3f137010e4
Laura de Oliveira <l4ur4.88@gmail.com>
2025-07-12 14:59:13 -0300

    feat: Generate audio embeddings and save it in database

--- server/src/http/routes/upload-audio.ts
+++ server/src/http/routes/upload-audio.ts
@@ -1,6 +1,8 @@
 import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod';
 import z from 'zod/v4';
-import { transcribeAudio } from '../../services/gemini.ts';
+import { db } from '../../../infra/db/connections.ts';
+import { schema } from '../../../infra/db/schema/index.ts';
+import { generateEmbeddings, transcribeAudio } from '../../services/gemini.ts';
 
 export const uploadAudioRoute: FastifyPluginCallbackZod = (app) => {
   app.post(
@@ -24,8 +26,21 @@ export const uploadAudioRoute: FastifyPluginCallbackZod = (app) => {
       const audioAsBase64 = audioBuffer.toString('base64');
 
       const transcription = await transcribeAudio(audioAsBase64, audio.mimetype);
+      const embeddings = await generateEmbeddings(transcription);
 
-      return { transcription }
+      const result = await db.insert(schema.audioChunks).values({
+        roomId,
+        transcription,
+        embeddings,
+      }).returning();
+
+      const chunk = result[0];
+
+      if (!chunk) {
+        throw new Error('Failed to save audio chunk.');
+      }
+
+      return reply.status(201).send({ chunkId: chunk.id });
     }
   );
 };
